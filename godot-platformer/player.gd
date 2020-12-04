22edcbe464e7ff5b91603c75536f27e4e8ed5081
Meghpal <meghpal.s@gmail.com>
2020-12-04 23:52:31 +0530

    force and animations

--- godot-platformer/player.gd
+++ godot-platformer/player.gd
@@ -2,21 +2,40 @@ extends KinematicBody2D
 
 var motion = Vector2()
 const UP = Vector2(0,-1)
-export var speed = 200
+export var max_speed = 200
 export var gravity = 10
+export var acceleration = 50
+export var friction = 0.2
 export var jump_force = 400
 
 func _physics_process(delta):
+	var has_friction = false
 	motion.y += gravity
+	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = speed
+		motion.x = min(motion.x + acceleration, max_speed)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -speed
+		motion.x = max(motion.x - acceleration, -max_speed)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		$Sprite.play("Idle")
+		has_friction = true
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -jump_force
+		if has_friction == true:
+			motion.x = lerp(motion.x, 0, friction)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if has_friction == true:
+			motion.x = lerp(motion.x, 0, friction * 0.25)
 	
 	motion = move_and_slide(motion, UP)
+	pass
